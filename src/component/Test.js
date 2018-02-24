073d8746955a150b0afe26259ddf351b4d62b42c
lauque_m <lauque_m@epitech.eu>
2018-02-24 19:00:41 +0100

    ok

--- src/component/Test.js
+++ src/component/Test.js
@@ -1,19 +1,18 @@
+
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import { Navigation } from 'react-native-navigation';
 
-import { View, Text } from 'react-native';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  Button
+} from 'react-native';
 
-type Props = {};
-class Test extends Component<Props> {
-
-  render() {
-    return (
-      <View>
-        <Text>
-        BONSOIR
-        </Text>
-      </View>
-    );
-  }
-
-}
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' +
+    'Cmd+D or shake for dev menu',
+  android: 'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
